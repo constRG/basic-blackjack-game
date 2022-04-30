af9447e818152ff19ab1d2104bed1dd7514a848d
Ricardo L. Garcia <103750848+constRG@users.noreply.github.com>
2022-04-29 22:39:26 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let messageAlert = document.querySelector('.message-alert')
+let cardsResult = document.querySelector('.cards-result')
+let sumResult = document.querySelector('.sum-result')
+let startingCardsBtn = document.querySelector('#starting-cards-btn')
+let drawNewCardBtn = document.querySelector('#draw-new-card-btn')
+let restartGameBtn = document.querySelector('#restart-game-btn')
+
+let cards = []
+let sum = 0
+let isBlackjack = false
+let isAlive = false
+let messageAlertStr = ""
+
+function getRandomCardNumbers() {
+    let randomCardNumber = Math.floor(Math.random() * 13) + 1
+    if (randomCardNumber > 10) {
+        return 10
+    } else if (randomCardNumber === 1) {
+        return 11
+    } else {
+        return randomCardNumber
+    }
+}
+
+startingCardsBtn.addEventListener('click', () => {
+    isAlive = true
+    let firstCard = getRandomCardNumbers()
+    let secondCard = getRandomCardNumbers()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    startingCards()
+})
+
+function startingCards() {
+    cardsResult.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsResult.textContent += cards[i] + " "
+    }
+    sumResult.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        messageAlertStr = "Do you want to get a card?"
+    } else if (sum === 21) {
+        isBlackjack = true
+        messageAlertStr = "Wow! you've got Blackjack!"
+    } else {
+        messageAlertStr = "You're out of the game!"
+        isAlive = false
+    }
+    messageAlert.textContent = messageAlertStr
+}
+
+drawNewCardBtn.addEventListener('click', () => {
+    drawNewCard()
+})
+
+function drawNewCard() {
+    if (isAlive == true && isBlackjack == false) {
+        let newCard = getRandomCardNumbers()
+        sum += newCard
+        cards.push(newCard)
+        startingCards()
+    } else {
+        alert("Please restart the game or click starting cards again. Thank you.")
+    }
+}
+
+restartGameBtn.addEventListener('click', () => {
+    window.location.reload()
+})
+
